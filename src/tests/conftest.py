4798d6eccfe13a008225c4f29312c0e7c0f8a9cd
Sjats <149076885+Sjats@users.noreply.github.com>
2024-09-19 13:22:00 +0000

    test

---
+++ src/tests/conftest.py
@@ -0,0 +1,16 @@
+from src.classes.multipolygone import MultiPolygone
+import pytest
+
+
+# Multipolygones
+
+@pytest.fixture
+def multipolygone_simple():
+    return {"contour": [[[(0, 0), (0.1), (1, 1), (1, 0)]]]
+            }
+
+
+def pytest_configure():
+
+    # Multipolygones
+    pytest.multipolygone_simple = MultiPolygone(contour=[[[(0, 0), (0.1), (1, 1), (1, 0)]]])
--- src/tests/test_multipolygone.py
+++ src/tests/test_multipolygone.py
@@ -1,3 +1,23 @@
-from classes.multipolygone import MultiPolygone
+import pytest
+from src.classes.multipolygone import MultiPolygone
 
-a = MultiPolygone([[[(0, 0), (1, 1), (1, 0)]]])
+
+# Test que utilitza la fixture multipolygone_simple
+def test_multipolygone_simple_fixture(multipolygone_simple):
+    multipolygone = multipolygone_simple["contour"]
+    # Escriu un test que faci alguna cosa amb el multipolygone
+    assert len(multipolygone) == 1  # Comprova que hi ha un polygone principal
+    assert len(multipolygone[0]) == 1  # Comprova que no hi ha inclaus
+
+
+# Test que utilitza pytest.multipolygone_simple
+def test_multipolygone_global():
+    # Accedeix al multipolygone creat en pytest_configure
+    multipolygone = pytest.multipolygone_simple
+    assert isinstance(multipolygone, MultiPolygone)
+
+    # Verifica la propietat contour
+    assert len(multipolygone.contour) == 1
+    assert (
+        len(multipolygone.contour[0]) == 1
+    )  # Assegura't que hi ha només el polygone principal
