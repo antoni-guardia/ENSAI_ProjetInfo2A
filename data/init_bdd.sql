47a5ea47b21e0aa8f3cc9feafc97702c52ad8c50
Sjats <149076885+Sjats@users.noreply.github.com>
2024-09-22 09:58:40 +0000

    init_sql

---
+++ data/init_bdd.sql
@@ -0,0 +1,20 @@
+CREATE TABLE MultiPolygone (
+    id_multipolygone SERIAL PRIMARY KEY,
+    contour GEOMETRY -- PostGIS extension à savoir utiliser
+);
+
+CREATE TABLE Zone (
+    id_zone SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    id_multipolygone INT NOT NULL,
+    id_zones_fille INT[] DEFAULT NULL, -- Array to store child zone IDs, if any
+    FOREIGN KEY (id_multipolygone) REFERENCES MultiPolygone(id_multipolygone)
+);
+
+
+CREATE TABLE Zonage (
+    id_zonage SERIAL PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    id_zonage_mere INT DEFAULT NULL,
+    annee VARCHAR(255) NOT NULL, -- Array to store child zone IDs, if any
+);
