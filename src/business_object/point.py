aa0206ffbc9d58a1c41990ff340adebc78246758
Chloé Fournereau <chloe.fournereau@eleve.ensai.fr>
2024-10-11 14:19:20 +0200

    class point

--- src/business_object/point.py
+++ src/business_object/point.py
@@ -1,3 +1,8 @@
 class Point:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, x, y) -> None:
+        self._x = x
+        self._y = y
+    
+    @property
+    def point(self):
+        return self._x, self._y
