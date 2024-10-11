9532927eae00d43f31698556050d0163dd76e3c1
Sidonie CLEUX <sido.cleux@gmail.com>
2024-10-11 14:43:19 +0200

    contour

--- src/business_object/contour.py
+++ src/business_object/contour.py
@@ -1,3 +1,18 @@
+from Point import Point
+
+
 class Contour:
-    def __init__(self) -> None:
+    """
+    Classe définissant le contour, indique
+    si un point est à l'intérieur du contour ou non
+
+    Attributs
+    ----------
+    point : Liste
+        point
+    """
+    def __init__(self, points: List[Point]) -> None:
+        self.points = points
+
+    def est_dedans(self, point: Tuple[float, float]) -> bool:
         pass
