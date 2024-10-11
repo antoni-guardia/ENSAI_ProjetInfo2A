77f1cc68bcbccd24c87bde598f595d6f6d979d33
Chloé Fournereau <chloe.fournereau@eleve.ensai.fr>
2024-10-11 14:52:32 +0200

    flake8

--- src/business_object/point.py
+++ src/business_object/point.py
@@ -1,5 +1,6 @@
 class Point:
-  def __init__(self, x: float, y: float) -> None:
+
+    def __init__(self, x: float, y: float) -> None:
         """
         Initialise un point avec des coordonnées x et y.
         Les coordonnées doivent être des nombres flottants (float).
@@ -7,12 +8,12 @@ class Point:
         :param x: La coordonnée x du point (doit être un float).
         :param y: La coordonnée y du point (doit être un float).
         :raises TypeError: Si x ou y n'est pas un float.
-        """
+            """
         if not isinstance(x, float) or not isinstance(y, float):
             raise TypeError("Les coordonnées x et y doivent être des nombres flottants.")
         self._x = x
         self._y = y
-        
+
     @property
     def x(self):
         """
